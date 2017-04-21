befb27b99504824d9538478a0c9f7350fac34151
alexthegreat63 <alexkearns6@gmail.com>
2017-04-20 22:17:26 -0400

    Updated loadData and visualize_data with Fourier transform

--- Resources/visualize_data.m
+++ Resources/visualize_data.m
@@ -3,14 +3,14 @@ clear
 close all
 
 %% Load data
-load('../data.mat')
+load('./data.mat')
+
+%% Plot channels
 sample_rate = 1000;
 dur = 310000 / sample_rate;
 dur_test = 147500 / sample_rate;
 t_train = linspace(0, dur, 310000);
 t_test = linspace(0, dur_test, 147500);
-
-%% Plot channels
 
 for i = 1:62
     figure
@@ -21,3 +21,20 @@ end
 
 %% Spectrogram
 % https://www.mathworks.com/matlabcentral/answers/265969-how-to-understand-spectrogram-function
+% for i = 1:62
+%     figure
+%     plot(t_train, sub1_ecog(:,i) * 1e-3, 'b');
+%     hold on
+%     plot(t_train, sub1_glove(:,1), 'r');
+% end
+
+%% Fourier Transform
+
+fs = 1000; % sampling frequency
+y = fft(sub1_ecog(:,2));         
+m = abs(y);                
+p = angle(y);
+f = (0:length(y)-1)*fs/length(y);
+% Plot
+figure; subplot(2,1,1); plot(f,m);  title('Magnitude'); xlabel('freq (Hz)');
+subplot(2,1,2); plot(f,rad2deg(p)); title('Phase'); xlabel('freq (Hz)');
--- loadData.m
+++ loadData.m
@@ -3,17 +3,17 @@ clear
 close all
 
 %% Subject 1
-session = IEEGSession('I521_A0012_D001', 'shashank', 'sha_ieeglogin.bin');
+session = IEEGSession('I521_A0012_D001', 'ak', 'ieeglogin.bin');
 sample_rate = session.data.rawChannels(1).sampleRate;
 n_samples = 310000;
 n_chan = 62;
 sub1_ecog = session.data.getvalues(1:n_samples, 1:n_chan);
 
-session = IEEGSession('I521_A0012_D002', 'shashank', 'sha_ieeglogin.bin');
+session = IEEGSession('I521_A0012_D002', 'ak', 'ieeglogin.bin');
 n_chan = 5;
 sub1_glove = session.data.getvalues(1:n_samples, 1:n_chan);
 
-session = IEEGSession('I521_A0012_D003', 'shashank', 'sha_ieeglogin.bin');
+session = IEEGSession('I521_A0012_D003', 'ak', 'ieeglogin.bin');
 n_samples = 147500;
 n_chan = 62;
 sub1_ecog_test = session.data.getvalues(1:n_samples, 1:n_chan);
@@ -21,17 +21,17 @@ sub1_ecog_test = session.data.getvalues(1:n_samples, 1:n_chan);
 
 %% Subject 2
 
-session = IEEGSession('I521_A0013_D001', 'shashank', 'sha_ieeglogin.bin');
+session = IEEGSession('I521_A0013_D001', 'ak', 'ieeglogin.bin');
 sample_rate = session.data.rawChannels(1).sampleRate;
 n_samples = 310000;
 n_chan = 48;
 sub2_ecog = session.data.getvalues(1:n_samples, 1:n_chan);
 
-session = IEEGSession('I521_A0013_D002', 'shashank', 'sha_ieeglogin.bin');
+session = IEEGSession('I521_A0013_D002', 'ak', 'ieeglogin.bin');
 n_chan = 5;
 sub2_glove = session.data.getvalues(1:n_samples, 1:n_chan);
 
-session = IEEGSession('I521_A0013_D003', 'shashank', 'sha_ieeglogin.bin');
+session = IEEGSession('I521_A0013_D003', 'ak', 'ieeglogin.bin');
 n_samples = 147500;
 n_chan = 48;
 sub2_ecog_test = session.data.getvalues(1:n_samples, 1:n_chan);
@@ -39,19 +39,20 @@ sub2_ecog_test = session.data.getvalues(1:n_samples, 1:n_chan);
 
 %% Subject 3
 
-session = IEEGSession('I521_A0014_D001', 'shashank', 'sha_ieeglogin.bin');
+session = IEEGSession('I521_A0014_D001', 'ak', 'ieeglogin.bin');
 sample_rate = session.data.rawChannels(1).sampleRate;
 n_samples = 310000;
 n_chan = 64;
 sub3_ecog = session.data.getvalues(1:n_samples, 1:n_chan);
 
-session = IEEGSession('I521_A0014_D002', 'shashank', 'sha_ieeglogin.bin');
+session = IEEGSession('I521_A0014_D002', 'ak', 'ieeglogin.bin');
 n_chan = 5;
 sub3_glove = session.data.getvalues(1:n_samples, 1:n_chan);
 
-session = IEEGSession('I521_A0014_D003', 'shashank', 'sha_ieeglogin.bin');
+session = IEEGSession('I521_A0014_D003', 'ak', 'ieeglogin.bin');
 n_samples = 147500;
 n_chan = 64;
 sub3_ecog_test = session.data.getvalues(1:n_samples, 1:n_chan);
 
+%%
 save('data', 'sub1_ecog', 'sub1_ecog_test', 'sub1_glove', 'sub2_ecog', 'sub2_ecog_test', 'sub2_glove', 'sub3_ecog', 'sub3_ecog_test', 'sub3_glove')
